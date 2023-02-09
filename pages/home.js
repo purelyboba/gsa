a2a806eb835dfaaa31f72cd8fd63cd204d6229bf
Ritvik Gupta <86858242+R1tzG@users.noreply.github.com>
2023-02-08 21:42:49 -0500

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -12,7 +12,7 @@ export default function HomePage() {
         <Page
           id="home"
           title="8405 Millburn Robotics"
-          p="8405 Millburn Robotics is a FIRST® Tech Challenge (FTC) team based at Millburn High School in Millburn, New Jersey. FTC is a nonprofit robotics competition where teams build and program robots to compete, inspiring students in STEM through hands-on learning and direct experience in the competition. We strive to exhibit gracious professionalism in all that we do as an FTC Competition. Aside from the competition itself, our team members also participate in outreach events, such as EssexIQ, a nonprofit where we pass on robotics skills we have learned through FTC."
+          p="8405 Millburn Robotics is a FIRST® Tech Challenge (FTC) team based at Millburn High School in Millburn, New Jersey. FTC is a nonprofit robotics competition where teams build and program robots to compete, inspiring students in STEM through hands-on learning and direct experience in the competition. We strive to exhibit gracious professionalism in all that we do as an FTC team. Aside from the competition itself, our team members also participate in outreach events, such as EssexIQ, a nonprofit where we pass on robotics skills we have learned through FTC."
         />
       </main>
     </div>
