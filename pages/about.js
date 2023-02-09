7e069c92b1111759f5b9858a55d8b9610fbf4824
kingKedSha <kedaar.s@gmail.com>
2023-02-08 21:38:21 -0500

    lorem ipsum dolor sit anem

--- pages/about.js
+++ pages/about.js
@@ -12,7 +12,7 @@ export default function AboutPage() {
         <Page
           id="about"
           title="About"
-          p="Our team consists of 15 members across grades at Millburn High School. This is our first year back to FTC after a 2 year break."
+          p="Our robotics team consists of 15 members across all four grades at Millburn High School. This is our first year back to FTC after a 2 year break, and all but one of our members are new to FTC. However, many of our members have previous robotics experience from other competitions like our school's VEX and VEXAI teams. This past experience gives our team a diverse skillset and profiency in many areas of STEM, such as AI and machine learning. Our team is composed of 9 builders and 6 coders, but many of us also have other roles like driving the robot, and managing outreach. CAD and the production of custom parts through machining is also important to the team. "
         />
       </main>
     </div>
--- pages/home.js
+++ pages/home.js
@@ -12,7 +12,7 @@ export default function HomePage() {
         <Page
           id="home"
           title="8405 Millburn Robotics"
-          p="8405 Millburn Robotics is a FIRST® Tech Challenge team based at Millburn High School in Millburn, New Jersey. FIRST® is a nonprofit inspiring students to become future leaders in stem through unique, hands-on programs worldwide. Despite being the first year in FTC for all but one of our members, our previous robotics experience drives our mission. Our experience with AI and machine learning spurs us to create with the elimination of human error and optimization of menial tasks in mind. We strive to build with the same ideals of optimization and reliability; whether that be through CAD, discussion or illustration."
+          p="8405 Millburn Robotics is a FIRST® Tech Challenge (FTC) team based at Millburn High School in Millburn, New Jersey. FTC is a nonprofit robotics competition where teams build and program robots to compete, inspiring students in STEM through hands-on learning and direct experience in the competition. We strive to exhibit gracious professionalism in all that we do as an FTC Competition. Aside from the competition itself, our team members also participate in outreach events, such as EssexIQ, a nonprofit where we pass on robotics skills we have learned through FTC."
         />
       </main>
     </div>
