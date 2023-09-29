34a017090b16e813393c47d7680d745c455c2800
Ritvik Gupta <ritvikgupta011@gmail.com>
2023-09-29 19:34:29 -0400

    img update

--- pages/home.js
+++ pages/home.js
@@ -21,7 +21,7 @@ export default function HomePage() {
         <div className={styles.mainsection} id={styles.maindesc}>
           <p>The Garden State Alliance is a distinguished collective of First Tech Challenge (FTC) teams based in New Jersey, dedicated to furthering the ideals of cooperative competition and community involvement. Emphasizing both technological advancement and community connection, this alliance not only competes at a high level but also organizes an array of outreach events to engage the public and inspire future generations. Recognizing that preparation and practice are paramount, the alliance also hosts scrimmages, fostering a collaborative environment where teams can test their robotic designs, share strategies, and refine their skills. Through these endeavors, The Garden State Alliance exemplifies the true spirit of FTC, blending competition with camaraderie, and innovation with outreach.</p>
           <Link id={styles.pagenav} href="https://discord.gg/8XKe3gyCZG" target='blank'>
-            <button>join now</button>
+            <button>JOIN NOW</button>
           </Link>
         </div>
         <div className={styles.subsection} id={styles.qrcode}>
